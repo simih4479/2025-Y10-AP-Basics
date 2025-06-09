dab7a0fa7d560fde5798fa7389435af5da81f192
simih4479 <simih4479@masseyhigh.school.nz>
2025-06-09 14:12:16 +1200

    Variable Basics

---
+++ P_02_Variables.py
@@ -0,0 +1,14 @@
+# Code to learn about variables...
+
+# Variables can be a string (Note the speech marks)
+greeting = "hello world"
+
+# Variables can be a number
+fav_num = 21
+num_string = "5"
+
+
+# constants are uppercase
+PI = 3.14
+
+a_01_my_num = 21
